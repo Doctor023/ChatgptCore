4f8ebcba7c1b40882b3cd66e8f1e0ad42893ead3
Doctor023 <109444069+Doctor023@users.noreply.github.com>
2023-12-31 00:56:07 +0300

    Добавьте файлы проекта.

---
+++ ChatgptCore/Controllers/RequestController.cs
@@ -0,0 +1,15 @@
+﻿using ChatgptCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ChatgptCore.Controllers
+{
+    public class RequestController : Controller
+    {
+        [HttpPost]
+        public IActionResult Send()
+        {
+            return Json("Отправлено");
+        }
+    }
+}
---
+++ ChatgptCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatgptCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
