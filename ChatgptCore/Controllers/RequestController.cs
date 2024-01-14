bc704a9d227d99387a70d123fa4948c893928bcf
Doctor023 <109444069+Doctor023@users.noreply.github.com>
2024-01-14 19:35:19 +0300

    fixed test response to request

--- ChatgptCore/Controllers/RequestController.cs
+++ ChatgptCore/Controllers/RequestController.cs
@@ -7,11 +7,13 @@ namespace ChatgptCore.Controllers
     public class RequestController : Controller
     {
         [HttpPost]
-        public async Task<IActionResult> SendAsync([FromBody] RequestGptModel requestGpt)
+        public async Task<IActionResult> SendAsync([FromBody] RequestGptModel requestGptModel)
         {
-            ChatGPT chatGPT = new ChatGPT();
+            /*ChatGPT chatGPT = new ChatGPT();
             string? response = await chatGPT.SendRequest();
-            return Json(response);
+            return Json(response);*/
+            Console.WriteLine(requestGptModel.Message);
+            return Json(new { requestGptModel.Id, requestGptModel.Message });
         }
     }
 }
